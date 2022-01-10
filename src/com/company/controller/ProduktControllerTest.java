e698461d204e0c0ed915b7332e0f71d5605dd55e
Vlad <vladfilip.629@gmail.com>
2022-01-10 18:40:20 +0200

    2

--- src/com/company/controller/ProduktControllerTest.java
+++ src/com/company/controller/ProduktControllerTest.java
@@ -2,6 +2,8 @@ package com.company.controller;
 
 import com.company.model.Produkt;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProduktControllerTest {
@@ -34,14 +36,22 @@ class ProduktControllerTest {
 
     @org.junit.jupiter.api.Test
     void updateProdukt() {
+        produktController.addProdukt("p1",10,"sku1",6);
+        List<Produkt> newList = produktController.getAll();
+        assertEquals(newList.get(0).getAnzahl(), 6);
+        produktController.updateProdukt("p1",5);
+        assertEquals(newList.get(0).getAnzahl(), 11);
+        produktController.updateProdukt("p1",-4);
+        assertEquals(newList.get(0).getAnzahl(), 7);
     }
 
     @org.junit.jupiter.api.Test
     void getAll() {
       assertEquals(produktController.getAll().size(),0);
+        produktController.addProdukt("p1",10,"sku1",6);
+        produktController.addProdukt("p2",10,"sku2",10);
+        produktController.addProdukt("p3",12,"sku3",10);
+        assertEquals(produktController.getAll().size(),3);
     }
 
-    @org.junit.jupiter.api.Test
-    void findByName() {
-    }
 }
